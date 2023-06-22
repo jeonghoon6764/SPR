88fdceeb6dc36f82915400a2bb4be2ab06c4fb4f
jeonghoon6764 <iampressured@gmail.com>
2023-06-22 01:02:02 -0400

    Add test case for discount policy

---
+++ SpringPractice/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,37 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP should get discount rate 10%")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000); //alt  + enter to import (mac = option)
+    }
+
+    @Test
+    @DisplayName("if user is not the VIP, then discount should not be applied")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+
+}
