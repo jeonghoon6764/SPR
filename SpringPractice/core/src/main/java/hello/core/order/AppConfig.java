969951856fa23379a5dbe681b45b9336e0ea3dd2
jeonghoon6764 <iampressured@gmail.com>
2023-06-22 22:22:15 -0400

    Change to Spring

--- SpringPractice/core/src/main/java/hello/core/order/AppConfig.java
+++ SpringPractice/core/src/main/java/hello/core/order/AppConfig.java
@@ -1,19 +1,36 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 //makes object for application
+@Configuration
 public class AppConfig {
     // Constructor Injection
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
+    /////////////
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
