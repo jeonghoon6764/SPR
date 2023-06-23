fcba37c392ef2b04de705a223f13b9d2cabc8379
jeonghoon6764 <iampressured@gmail.com>
2023-06-22 20:37:05 -0400

    ADD appconfig class

---
+++ SpringPractice/core/src/main/java/hello/core/order/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+
+//makes object for application
+public class AppConfig {
+    // Constructor Injection
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
